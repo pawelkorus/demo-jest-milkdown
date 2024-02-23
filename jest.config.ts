cc42b4ae8dad81cfc8936b0b358accaf2a9921d4
pawelkorus <pawelkorus@ymail.com>
2024-02-23 21:44:20 +0000

    working jest-babel configuration

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,16 @@ import type { Config } from 'jest'
 
 const config: Config = {
   preset: 'ts-jest',
+  transform: {
+    "^.+\\.[t]sx?$": "ts-jest",
+    "^.+\\.[j]sx?$": "babel-jest"
+  },
+  transformIgnorePatterns: [
+  ],
+  moduleNameMapper: {
+    "^@milkdown/prose/(.*)$": "<rootDir>/node_modules/@milkdown/prose/lib/$1.js",
+    "^@milkdown/(.*)$": "<rootDir>/node_modules/@milkdown/$1"
+  },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   injectGlobals: true,
